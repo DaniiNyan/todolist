18a1cc0180b13bfc1a73671e0779fae95b985a9f
daniinyan <daniela.araujo@ilegra.com>
2019-06-18 15:38:04 -0300

    1/Cria classe ToDoList

---
+++ src/main/java/com/daniinyan/todolist/domain/ToDoList.java
@@ -0,0 +1,27 @@
+package com.daniinyan.todolist.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ToDoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public ToDoList() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
